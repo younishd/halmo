1f10c6819fb61f30fa3659c59f2053a3fe10743b
Younis <younis.bensalah@gmail.com>
2020-12-27 16:22:14 +0100

    path.lua and version.lua

---
+++ app/path.lua
@@ -0,0 +1,9 @@
+--[[
+--
+-- Halmö
+--
+-- (c) 2015-2020 Younis Bensalah <younis.bensalah@gmail.com>
+--
+--]]
+
+return package.path .. ';src/?.lua;lib/?.lua'
---
+++ app/version.lua
@@ -0,0 +1,9 @@
+--[[
+--
+-- Halmö
+--
+-- (c) 2015-2020 Younis Bensalah <younis.bensalah@gmail.com>
+--
+--]]
+
+return (function() for v in io.lines('VERSION') do return v end end)()
