98763d77a2209cbabbe59a162ece09395a7c7b1c
Younis <younis.bensalah@gmail.com>
2023-02-01 15:49:02 +0100

    Add gen-loader script

--- app/env.lua
+++ app/env.lua
@@ -6,31 +6,44 @@
 --
 --]]
 
-package.path                = package.path .. ';src/?.lua;lib/?.lua'
-version                     = (function() for v in io.lines('VERSION') do return v end end)()
-platform                    = function() return string.format("platform: %s | %s | %s | %s %s | %s", love.system.getOS(), _VERSION, string.format("LÖVE %d.%d.%d (%s)", love.getVersion()), ({love.graphics.getRendererInfo()})[1], ({love.graphics.getRendererInfo()})[2], ({love.graphics.getRendererInfo()})[4]) end
-class                       = require 'middleclass.middleclass'
-local _table                = require 'helpers.table'
-table.explode               = _table.explode
-table.contains              = _table.contains
-table.append                = _table.append
-local _math                 = require 'helpers.math'
-math.sign                   = _math.sign
-math.isint                  = _math.isint
-mixins                      = {}
-mixins.hooks                = require 'mixins.hooks'
-log                         = require 'helpers.log'
-map                         = require 'helpers.map'
-imap                        = require 'helpers.imap'
-partial                     = require 'helpers.partial'
-times                       = require 'helpers.times'
-kpairs                      = require 'helpers.kpairs'
-Game                        = require 'Game'
-Engine                      = require 'core.Engine'
-Board                       = require 'core.Board'
-BoardUI                     = require 'ui.BoardUI'
-MenuUI                      = require 'ui.MenuUI'
-logo                        = function() log.info([[
+package.path = package.path .. ";src/?.lua;lib/?.lua"
+version = (function()
+  for v in io.lines("VERSION") do
+    return v
+  end
+end)()
+platform = function()
+  return string.format(
+    "platform: %s | %s | %s | %s %s | %s",
+    love.system.getOS(),
+    _VERSION,
+    string.format("LÖVE %d.%d.%d (%s)", love.getVersion()),
+    ({love.graphics.getRendererInfo()})[1],
+    ({love.graphics.getRendererInfo()})[2],
+    ({love.graphics.getRendererInfo()})[4]
+  )
+end
+class = require "middleclass.middleclass"
+local _table = require "helpers.table"
+table.explode = _table.explode
+table.contains = _table.contains
+table.append = _table.append
+local _math = require "helpers.math"
+math.sign = _math.sign
+math.isint = _math.isint
+mixins = {}
+mixins.hooks = require "mixins.hooks"
+log = require "helpers.log"
+map = require "helpers.map"
+imap = require "helpers.imap"
+partial = require "helpers.partial"
+times = require "helpers.times"
+kpairs = require "helpers.kpairs"
+Game = require "Game"
+require "loader"
+logo = function()
+  log.info(
+    [[
 
 
                              ,,                    ,,  ,,
@@ -43,4 +56,6 @@ logo                        = function() log.info([[
     .JMML.  .JMML.`Moo9^Yo..JMML..JMML  JMML  JMML.`Ybmd9'
 
 
-]]) end
+]]
+  )
+end
