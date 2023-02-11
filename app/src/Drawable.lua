dc35bfdd3c15c43b52a8b639a7bee30c2fa83a81
Younis Bensalah <younis.bensalah@gmail.com>
2023-02-11 21:51:25 +0100

    Drawable

---
+++ app/src/Drawable.lua
@@ -0,0 +1,29 @@
+--[[
+--
+-- Halmö
+--
+-- (c) 2015-2023 Younis Bensalah <younis.bensalah@gmail.com>
+--
+--]]
+local Drawable = class("Drawable")
+
+Drawable:include(mixins.hooks)
+
+function Drawable:initialize()
+    assert(false, "class is abstract!")
+end
+
+function Drawable:draw()
+    assert(false, "class is abstract!")
+end
+
+function Drawable:on_mouse_update(x, y)
+end
+
+function Drawable:on_mouse_pressed(x, y, button)
+end
+
+function Drawable:on_mouse_released(x, y, button)
+end
+
+return Drawable
